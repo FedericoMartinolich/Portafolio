bc437f05d8e2c7c78bf8c6183ece9f0f9b55f21e
Federico José Martinolich <martinolichfederico@gmail.com>
2026-08-04 14:19:30 -0300

    Update: Rebrit start year to 2025

--- src/components/TagList.vue
+++ src/components/TagList.vue
@@ -1,6 +1,8 @@
 <template>
   <ul v-if="items.length" class="tag-list">
-    <li v-for="(item, i) in items" :key="i" class="tag-list__item">{{ item }}</li>
+    <li v-for="(item, i) in items" :key="i" class="tag-list__item">
+      {{ item }}
+    </li>
   </ul>
 </template>
 
@@ -8,19 +10,20 @@
 defineProps({
   items: {
     type: Array,
-    default: () => []
-  }
-})
+    default: () => [],
+  },
+});
 </script>
 
 <style scoped>
 .tag-list {
   list-style: none;
   margin: 0;
-  padding: 0;
+  padding: 1.1rem 1.4rem 1.4rem;
   display: flex;
   flex-wrap: wrap;
   gap: 0.45rem;
+  justify-content: center;
 }
 
 .tag-list__item {
